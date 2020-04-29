44373534827074e3266da53659054454b93a62ce
lsrbanna <64417115+lsrbanna@users.noreply.github.com>
2020-04-29 13:55:35 +0530

    Final project

---
+++ loginnew/src/main/java/com/example/demo/LoginnewApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+//@ComponentScan("com.in28minutes.springboot.web")
+public class LoginnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginnewApplication.class, args);
+	}
+
+}
